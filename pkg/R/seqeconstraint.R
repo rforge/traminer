ca0842a24574cc52dbb188b8b6b9eda9b1b5428b
matthias_studer <matthias_studer@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-05-12 09:25:26 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/traminer@118 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/seqeconstraint.R
+++ pkg/R/seqeconstraint.R
@@ -1,23 +1,35 @@
-seqeconstraint<-function(maxGap=-1, windowSize=-1, ageMin=-1, ageMax=-1,ageMaxEnd=-1, countMethod=1){
-  ret<-list()
-  ret$maxGap <- maxGap
-  ret$windowSize <- windowSize
-  ret$ageMin <- ageMin
-  ret$ageMax <- ageMax
-  ret$ageMaxEnd <-ageMaxEnd
-  ret$countMethod <- countMethod
-  class(ret)<-"seqeconstraint"
-  return(ret)
+seqeconstraint <- function(maxGap=-1, windowSize=-1, ageMin=-1, ageMax=-1, ageMaxEnd=-1, countMethod=1){
+	## check that all constraints are coherent
+	if(ageMaxEnd != -1 && ageMax == -1){
+		ageMax <- ageMaxEnd
+	}
+	if(maxGap!= -1 && windowSize!=-1 && maxGap>windowSize){
+		stop(" [!] maxGap is greater than windowSize")
+	}
+	if(ageMin!= -1 && ageMax!=-1 && ageMin>ageMax){
+		stop(" [!] ageMin is greater than ageMax or ageMaxEnd")
+	}
+	ret <- list()
+	ret$maxGap <- maxGap
+	ret$windowSize <- windowSize
+	ret$ageMin <- ageMin
+	ret$ageMax <- ageMax
+	ret$ageMaxEnd <- ageMaxEnd
+	ret$countMethod <- countMethod
+	class(ret) <- "seqeconstraint"
+	return(ret)
 }
 
-print.seqeconstraint<-function(x,...){
+print.seqeconstraint<-function(x, ...){
 	z<-data.frame(Constraint=names(x),Value=as.numeric(x))
-	z <- z[z$"Value"!=-1,]
-	if (z[z$"Constraint"=="countMethod","Value"] == 1) {
-		z[z$"Constraint"=="countMethod","Value"] <- "One by sequence"
+	z <- z[z$"Value"!=-1, ]
+	if (z[z$"Constraint"=="countMethod", "Value"] == 1) {
+		z[z$"Constraint"=="countMethod", "Value"] <- "One by sequence"
 	}
-	if (z[z$"Constraint"=="countMethod","Value"] == 2) {
-		z[z$"Constraint"=="countMethod","Value"] <-  "Several by sequence"
+	if (z[z$"Constraint"=="countMethod", "Value"] == 2) {
+		z[z$"Constraint"=="countMethod", "Value"] <-  "Several by sequence"
 	}
-	if(nrow(z) > 0) { print(z,row.names=FALSE,...) }
+	if(nrow(z) > 0) { 
+		print(z, row.names=FALSE,...) 
+	}
 }
