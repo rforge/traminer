2caa716d99a40857a86eb6b47480e42e5b3c5b50
gritschard <gritschard@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-02-17 17:42:50 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/traminer@156 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/TraMineRextras/R/rowmode.R
+++ pkg/TraMineRextras/R/rowmode.R
@@ -1,7 +1,7 @@
 ## returns the modal value of a variable
 ## author: Gilbert Ritschard
 
-rowmode <- function(v, except = "*") {
+rowmode <- function(v, except = NULL) {
 	tt <- table(v)
 	tt <- tt[!(names(tt) %in% except)]
 	return(names(tt)[which.max(tt)])
