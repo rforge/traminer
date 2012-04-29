93f06f7309f6f8db8641240f7281b619d6751cc3
matthias_studer <matthias_studer@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-04-29 19:21:44 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/traminer@220 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/WeightedCluster/R/zzz.R
+++ pkg/WeightedCluster/R/zzz.R
@@ -1,16 +1,23 @@
 
 .onAttach <- function(libname, pkgname){
-	suppressWarnings(descr<-utils:::packageDescription("WeightedCluster"))
-	Tver <- TraMineR:::extract.ver(descr$Version)
-	if(as.numeric(Tver[2])%%2==0) {
+	suppressWarnings(descr <- utils:::packageDescription("WeightedCluster"))
+	if(utils:::packageVersion("WeightedCluster")$minor %% 2 == 0) {
 		state <- "stable"
 	}
 	else {
 		state <- "development"
 	}
 	builtDate <- strsplit(strsplit(descr$Built, ";")[[1]][3], " ")[[1]][2]
-	packageStartupMessage("\nThis is ",descr$Package," ", state, " version ", descr$Version, " (Built: ", builtDate, ")")
-	packageStartupMessage('\nTo get the manuals, please run:\n   vignette("WeightedCluster") ## Complete manual in French\n   vignette("WeightedClusterPreview") ## Short preview in English\n')
-	packageStartupMessage("To cite WeightedCluster in publications please use:\n")
-	packageStartupMessage("Studer, Matthias (2012). Étude des inégalités de genre en début de carrière\n académique à l'aide de méthodes innovatrices d'analyse de données séquentielles,\n Chapitre: Le manuel de la librairie WeightedCluster : Un guide pratique pour la\n création de typologies de trajectoires en sciences sociales avec R. Thèse de doctorat inédite, Université de Genève.\n\n")
+	packageStartupMessage("This is WeightedCluster ", state, " version ", descr$Version, " (Built: ", builtDate, ")")
+	packageStartupMessage('\nTo get the manuals, please run:')
+	packageStartupMessage('   vignette("WeightedCluster") ## Complete manual in French')
+	packageStartupMessage('   vignette("WeightedClusterPreview") ## Short preview in English')
+	packageStartupMessage("\nTo cite WeightedCluster in publications please use:")
+	packageStartupMessage("Studer, Matthias (2012). \u00c9tude des in\u00e9galit\u00e9s de genre en d\u00e9but de carri\u00e8re")
+	packageStartupMessage("   acad\u00e9mique \u00e0 l\'aide de m\u00e9thodes innovatrices d\'analyse de donn\u00e9es")
+	packageStartupMessage("   s\u00e9quentielles, Chapitre: Le manuel de la librairie WeightedCluster :")
+	packageStartupMessage("   Un guide pratique pour la cr\u00e9ation de typologies de trajectoires en sciences")
+	packageStartupMessage("   sociales avec R. Th\u00e8se de doctorat in\u00e9dite, Universit\u00e9 de Gen\u00e8ve.\n")
 }
+
+.onAttach()
