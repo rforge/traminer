a288bf23e1003b620b77e925671e626249ef5550
matthias_studer <matthias_studer@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-08-14 11:12:44 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/traminer@256 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/TraMineRextras/R/seqstart.R
+++ pkg/TraMineRextras/R/seqstart.R
@@ -28,20 +28,10 @@ seqstart <- function(seqdata, data.start, new.start, tmin=NULL, tmax=NULL, missi
 	## cat(tmin, tmax, correction)
 	##new.index.mat <- new.index - tmin + 1
 	new.data <- matrix(as.character(missing), ncol=(tmax-tmin+1), nrow=nrow(seqdata))
-	seqindex <- matrix(tmin:tmax, ncol=(tmax-tmin+1), nrow=nrow(seqdata), byrow=TRUE)
-	seqdataindex <- matrix(1:ncol(seqdata), ncol=ncol(seqdata), nrow=nrow(seqdata), byrow=TRUE)
-	seqdata <- as.matrix(seqdata)
-	
-	rowindex <- (1:ncol(seqdata))-1
-	for(ind in 1:nrow(seqdata)){
-		indexes <-  new.index[ind]+rowindex - tmin +1
-		cond <- (indexes <= ncol(new.data)) & (indexes > 0)
-		## print(indexes)
-		new.data[ind, indexes[cond]] <- seqdata[ind, cond]
-		#print(seqdata[which.indiv, oldindex])
-		##print(new.data)
-	}
-	return(new.data)
+	seqdatadim <- dim(seqdata)
+	seqdata <- as.character(as.matrix(seqdata))
+	dim(seqdata) <- seqdatadim
+	return(.Call(TMREXTRAS_tmrextrasseqstart, seqdata, new.data, as.integer(new.index-tmin)))
 }
 
 
---
+++ pkg/TraMineRextras/src/seqstart.cpp
@@ -0,0 +1,26 @@
+
+
+extern "C" {
+
+
+#include <Rdefines.h>
+
+	SEXP tmrextrasseqstart(SEXP seqdata, SEXP new_data, SEXP new_indexS){
+		int nrow_seqdata=INTEGER(GET_DIM(seqdata))[0];
+		int ncol_seqdata=INTEGER(GET_DIM(seqdata))[1]; 
+		int nrow_new_data=INTEGER(GET_DIM(new_data))[0];
+		int ncol_new_data=INTEGER(GET_DIM(new_data))[1]; 
+		int * new_index=INTEGER(new_indexS);
+	//	rowindex <- (1:ncol(seqdata))-1
+		for(int i=0; i<nrow_seqdata; i++){
+			// indexes <-  new.index[ind]+rowindex - tmin +1
+			for(int j=0; j<ncol_seqdata;j++){
+				int new_j =  new_index[i]+j;
+				if(new_j>=0 && new_j<ncol_new_data){
+					SET_STRING_ELT(new_data, i+new_j*nrow_new_data, STRING_ELT(seqdata, i+j*nrow_seqdata));
+				}
+			}
+		}
+		return new_data;
+	}
+}
