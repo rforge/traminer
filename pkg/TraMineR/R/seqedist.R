942c83f88fa81c9c1fde3fd7d776745ba2b7fa47
matthias_studer <matthias_studer@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-11-15 20:06:07 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/traminer@294 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/TraMineR/R/seqedist.R
+++ pkg/TraMineR/R/seqedist.R
@@ -1,5 +1,9 @@
 seqedist<-function(seqe, idcost, vparam, interval=TRUE, norm=TRUE){
-    norm<-as.integer(norm)
-    interval<-as.integer(interval)
+    norm <- as.integer(norm)
+    interval <- as.integer(interval)
     return(.Call(TMR_tmrseqedist, seqe, idcost, vparam, norm, interval));
+}
+
+seqeage <- function(seqe, eventList){
+	return(.Call("tmreventinseq", seqe, as.integer(eventList), PACKAGE="TraMineR"))
 }
