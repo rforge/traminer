8a46b7b62fe8e7853d495a9c5949f34c1e12699f
matthias_studer <matthias_studer@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-02-24 18:04:07 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/traminer@383 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/WeightedCluster/R/zzz.R
+++ pkg/WeightedCluster/R/zzz.R
@@ -14,7 +14,7 @@
 	packageStartupMessage('   vignette("WeightedCluster-fr") ## Complete manual in French')
 	packageStartupMessage('   vignette("WeightedClusterPreview") ## Short preview in English')
 	packageStartupMessage("\nTo cite WeightedCluster in publications please use:")
-	packageStartupMessage("Studer, Matthias (2013).WeightedCluster Library Manual: A practical guide to")
+	packageStartupMessage("Studer, Matthias (2013). WeightedCluster Library Manual: A practical guide to")
 	packageStartupMessage("   creating typologies of trajectories in the social sciences with R.")
 	packageStartupMessage("   LIVES Working Papers, 24. doi: 10.12682/lives.2296-1658.2013.24")
 	
