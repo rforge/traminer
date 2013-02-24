e66805e04a41fe4674994fed122dd851b263fcd9
matthias_studer <matthias_studer@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-02-24 15:15:13 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/traminer@382 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/WeightedCluster/R/zzz.R
+++ pkg/WeightedCluster/R/zzz.R
@@ -14,9 +14,8 @@
 	packageStartupMessage('   vignette("WeightedCluster-fr") ## Complete manual in French')
 	packageStartupMessage('   vignette("WeightedClusterPreview") ## Short preview in English')
 	packageStartupMessage("\nTo cite WeightedCluster in publications please use:")
-	packageStartupMessage("Studer, Matthias (2012). \u00c9tude des in\u00e9galit\u00e9s de genre en d\u00e9but de carri\u00e8re")
-	packageStartupMessage("   acad\u00e9mique \u00e0 l\'aide de m\u00e9thodes innovatrices d\'analyse de donn\u00e9es")
-	packageStartupMessage("   s\u00e9quentielles, Chapitre: Le manuel de la librairie WeightedCluster :")
-	packageStartupMessage("   Un guide pratique pour la cr\u00e9ation de typologies de trajectoires en sciences")
-	packageStartupMessage("   sociales avec R. Th\u00e8se SES 777, Facult\u00E9 des sciences \u00E9conomiques\n   et sociales, Universit\u00e9 de Gen\u00e8ve.\n")
+	packageStartupMessage("Studer, Matthias (2013).WeightedCluster Library Manual: A practical guide to")
+	packageStartupMessage("   creating typologies of trajectories in the social sciences with R.")
+	packageStartupMessage("   LIVES Working Papers, 24. doi: 10.12682/lives.2296-1658.2013.24")
+	
 }
