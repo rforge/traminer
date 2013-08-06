819630f9228f7db32ef84afbf12c8b977a4d3e24
anthonvoyl <anthonvoyl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-08-06 12:26:59 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/traminer@448 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/TraMineR/R/zzz.R
+++ pkg/TraMineR/R/zzz.R
@@ -1,11 +1,16 @@
-## welcome message
+## .First.lib <- function(lib, pkg) {library.dynam("TraMineR", pkg, lib)}
 
 .onAttach <- function(libname, pkgname){
-	suppressWarnings(descr <- utils:::packageDescription("PST"))
-
+	suppressWarnings(descr<-utils:::packageDescription("TraMineR"))
+	Tver <- TraMineR:::extract.ver(descr$Version)
+	if(as.numeric(Tver[2])%%2==0) {
+		state <- "stable"
+	}
+	else {
+		state <- "development"
+	}
 	builtDate <- strsplit(strsplit(descr$Built, ";")[[1]][3], " ")[[1]][2]
-
 	packageStartupMessage("\n",descr$Package," ", state, " version ", descr$Version, " (Built: ", builtDate, ")")
 	packageStartupMessage("Website: ", descr$URL)
-	## packageStartupMessage("Please type 'citation(\"PST\")' for citation information.\n")
+	packageStartupMessage("Please type 'citation(\"TraMineR\")' for citation information.\n")
 }
