609a000f46077cf14ce8b995fb7fcacab45ffacb
matthias_studer <matthias_studer@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-12-13 16:22:52 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/traminer@503 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/WeightedCluster/R/zzz.R
+++ pkg/WeightedCluster/R/zzz.R
@@ -15,7 +15,7 @@
 	packageStartupMessage("This is WeightedCluster ", state, " version ", descr$Version, builtDate)
 	packageStartupMessage('\nTo get the manuals, please run:')
 	packageStartupMessage('   vignette("WeightedCluster") ## Complete manual in English')
-	packageStartupMessage('   vignette("WeightedCluster-fr") ## Complete manual in French')
+	packageStartupMessage('   vignette("WeightedClusterFR") ## Complete manual in French')
 	packageStartupMessage('   vignette("WeightedClusterPreview") ## Short preview in English')
 	packageStartupMessage("\nTo cite WeightedCluster in publications please use:")
 	packageStartupMessage("Studer, Matthias (2013). WeightedCluster Library Manual: A practical guide to")
