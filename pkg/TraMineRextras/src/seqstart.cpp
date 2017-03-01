47105efc999ed02a2430453c665c2a813c79b22a
gritschard <gritschard@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-03-01 12:47:53 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/traminer@817 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/TraMineRextras/src/seqstart.cpp
+++ pkg/TraMineRextras/src/seqstart.cpp
@@ -22,3 +22,26 @@ extern "C" {
 		return new_data;
 	}
 }
+
+#include <R.h>
+#include <Rinternals.h>
+#include <stdlib.h> // for NULL
+#include <R_ext/Rdynload.h>
+
+/* FIXME:
+   Check these declarations against the C/Fortran source code.
+*/
+
+/* .Call calls */
+extern SEXP tmrextrasseqstart(SEXP, SEXP, SEXP);
+
+static const R_CallMethodDef CallEntries[] = {
+    {"tmrextrasseqstart", (DL_FUNC) &tmrextrasseqstart, 3},
+    {NULL, NULL, 0}
+};
+
+void R_init_TraMineRextras(DllInfo *dll)
+{
+    R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
+    R_useDynamicSymbols(dll, FALSE);
+}
