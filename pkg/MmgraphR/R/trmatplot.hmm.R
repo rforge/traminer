73e4410599360201016b203dd026f1eb76166176
adamopoulou <adamopoulou@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-05-07 12:43:40 +0000

    trmatplot.hmm
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/traminer@944 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/MmgraphR/R/trmatplot.hmm.R
@@ -0,0 +1,31 @@
+#'@keywords internal
+trmatplot.hmm <- function (d, seed = NULL, 
+													rowconstraint = TRUE, morder = 1,
+													cspal = NULL, cpal = NULL, title = NULL,
+                	       	xlab =  NULL, ylab = NULL, ylim = NULL, 
+													xtlab = NULL, ytlab = NULL,
+                      		pfilter = NULL,
+                       		shade.col = "grey80",
+                       		num = 1,
+                       		hide.col = NULL,
+                       		lorder = NULL,
+													plot = TRUE,
+                       		verbose = FALSE, ...){
+
+  d <- d $ transition_probs
+ 
+   trmatplot.default ( d = d, seed = seed, 
+											rowconstraint = rowconstraint, morder = morder,
+											cspal = cspal, cpal = cpal, title = title, 
+											xlab = xlab, ylab = ylab, ylim = ylim, 
+											xtlab = xtlab, ytlab = ytlab,
+                      pfilter = pfilter,
+                      shade.col = shade.col,
+                      num = num,
+                      hide.col = hide.col,
+                      lorder = lorder,
+											plot = plot,
+                      verbose = verbose, ... )
+  
+}
+
