ef1290bbfbd03ac77fc0f2f85c875c325ccffdd4
gritschard <gritschard@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-07-04 16:00:32 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/traminer@958 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/TraMineRextras/R/seqgen.missing.R
+++ pkg/TraMineRextras/R/seqgen.missing.R
@@ -1,19 +1,19 @@
 seqgen.missing <- function(seqdata, p.cases=.1, p.left=.2, p.gaps=0, p.right=.3,
           mt.left="nr", mt.gaps="nr", mt.right="nr"){
-          
+
     n <- nrow(seqdata)
     lgth <- max(seqlength(seqdata))
     nr.l <- attr(seqdata, mt.left)
     nr.g <- attr(seqdata, mt.gaps)
     nr.r <- attr(seqdata, mt.right)
-    
+
     nm <- round(p.cases * n, 0)
     ## selecting cases
     idm <- sort(sample(1:n, nm))
-    rdu.l <- runif(n,min=0,max=p.right)
+    rdu.l <- runif(n,min=0,max=p.left)
     rdu.g <- runif(n,min=0,max=p.gaps)
-    rdu.r <- runif(n,min=0,max=p.left)
-    
+    rdu.r <- runif(n,min=0,max=p.right)
+
     for (i in idm){
         # gaps
         gaps <- sample(1:lgth, round(rdu.g[i] * lgth, 0))
@@ -23,10 +23,8 @@ seqgen.missing <- function(seqdata, p.cases=.1, p.left=.2, p.gaps=0, p.right=.3,
         if (nl>0) seqdata[i,1:nl] <- nr.l
         # right missing
         nr <- round(rdu.r[i] * lgth, 0)
-        if (nr>0) seqdata[i,(lgth-nr):lgth] <- nr.r
+        if (nr>0) seqdata[i,(lgth-nr+1):lgth] <- nr.r
     }
-    
+
 	return(seqdata)
 }
-
-
