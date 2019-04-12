9a6eb750358eda06e03c8d5ea96ffbb8af0c8705
gritschard <gritschard@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2019-04-12 08:32:26 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/traminer@1019 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/TraMineRextras/R/seqtabstocc.R
+++ pkg/TraMineRextras/R/seqtabstocc.R
@@ -1,15 +1,24 @@
 # Frequencies of state co-occurrence patterns
 
-seqtabstocc <- function(seqdata, ...){
-    ssort <- t(apply(seqdata, MARGIN=1, sort))
-    ssort.seq <- suppressMessages(seqdef(ssort)) ##, alphabet = alph, states=alph, labels=stlab(seqdata))
-    matchlab <- match(alphabet(ssort.seq),alphabet(seqdata))
-    ssort.seq <- suppressMessages(seqdef(ssort, labels=stlab(seqdata)[matchlab]))
-
-    sdss  <- suppressMessages(seqdef(seqdss(ssort.seq), missing="%"))
-
+seqtabstocc <- function(seqdata, with.missing=FALSE, ...){
+    m <- as.matrix(seqdata)
+    alphabet <- alphabet(seqdata)
+    misschar <- attr(seqdata, 'nr')
+    if (with.missing){
+      misschar <- attr(seqdata, 'void')
+      alphabet <- c(alphabet,attr(seqdata,'nr'))
+    }
+    else{
+      m[m==attr(seqdata,'void')] <- misschar
+    }
+    ssort <- t(apply(m, MARGIN=1, sort))
+    ssort.seq <- suppressMessages(seqdef(ssort, missing=misschar, nr=paste0(misschar,attr(seqdata,'nr')))) 
+    ##matchlab <- match(alphabet(ssort.seq),alphabet)
+    ##ssort.seq <- suppressMessages(seqdef(ssort, missing=attr(seqdata,'nr'), labels=stlab(seqdata)[matchlab]))
+    ##print(alphabet <- c(alphabet, "", attr(ssort.seq,"void"))) ##, attr(ssort.seq,"void")))
+    sdss  <- suppressMessages(seqdef(seqdss(ssort.seq)))
     tf <- seqtab(sdss, format="STS", ...)
     t <- attr(tf,"freq")
-    rownames(t) <- gsub("-\\*","",rownames(t))
+    ##rownames(t) <- gsub("-\\*","",rownames(t))
     return(t)
 }
