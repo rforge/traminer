dae5086b2bddb9b97c10937be950cca031cf8b42
gritschard <gritschard@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2019-11-13 09:25:07 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/traminer@1046 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/TraMineR/R/seqipos.R
+++ pkg/TraMineR/R/seqipos.R
@@ -1,36 +1,44 @@
 ## proportion of positive spells (or states)
 
-seqipos <- function(seqdata, dss=TRUE, pos.states=NULL, with.missing=FALSE){
+seqipos <- function(seqdata, dss=TRUE, pos.states=NULL, neg.states=NULL, with.missing=FALSE){
 
 	if (!inherits(seqdata,"stslist"))
 		stop(" [!] data is NOT a sequence object, see seqdef function to create one")
-  if (is.null(pos.states))
-		stop(" [!] pos.states is null, it should contain at least one valid state")
+  if (is.null(pos.states) & is.null(neg.states))
+		stop(" [!] at least one of pos.states and neg.states should be non null!")
 
   alph <- alphabet(seqdata)
   void <- attr(seqdata,"void")
 	nr <- attr(seqdata,"nr")
   if (with.missing)
     alph <- c(alph,nr)
-  if (!all(pos.states %in% alph)){
+
+  if (!is.null(pos.states) & !all(pos.states %in% alph)){
     stop(" [!] invalid values in pos.states: ",paste(pos.states[!pos.states %in% alph], collapse=",
     "))
   }
+  if (!is.null(pos.states) & !all(neg.states %in% alph)){
+    stop(" [!] invalid values in neg.states: ",paste(neg.states[!neg.states %in% alph], collapse=",
+    "))
+  }
+
+  if (is.null(pos.states)) pos.states <- alph[!alph %in% neg.states]
+  if (is.null(neg.states)) neg.states <- alph[!alph %in% pos.states]
 
   if (length(pos.states)!=length(unique(pos.states)))
     stop(" [!] Multiple occurrences of same state in pos.states")
+  if (length(neg.states)!=length(unique(neg.states)))
+    stop(" [!] Multiple occurrences of same state in neg.states")
 
-  if (with.missing)
-    recodes <- list("p"=pos.states, "!#%" = void)
-  else
-    recodes <- list("p"=pos.states, "*" = c(nr,void))
+  recodes <- list("p"=pos.states, "m"=neg.states)
 
   if (dss)
     s <- seqdss(seqdata, with.missing = with.missing)
   else
     s <- seqdata
 
-  sbinary <- suppressWarnings(seqrecode(s, recodes = recodes, otherwise="m"))
+  sbinary <- seqrecode(s, recodes = recodes, otherwise=attr(s,'void'))
+
   npos <- rowSums(sbinary=="p")
   nneg <- rowSums(sbinary=="m")
   ratio <- npos/(nneg + npos)
