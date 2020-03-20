661b28961f9c322df1f1b200bcafa88c08433329
gritschard <gritschard@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2020-03-20 08:35:20 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/traminer@1088 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/TraMineRextras/R/seqgranularity.R
+++ pkg/TraMineRextras/R/seqgranularity.R
@@ -1,44 +1,87 @@
 seqgranularity <- function(seqdata, tspan=3, method="last"){
 
-    metlist <- c("first","last","mostfreq")
-    if (!method %in% metlist) {
-        stop(" [!] method must be one of: ", paste(metlist, collapse = " "),
-            call. = FALSE)
+  metlist <- c("first","first.valid","last","last.valid","mostfreq")
+  if (!method %in% metlist) {
+      stop(" [!] method must be one of: ", paste(metlist, collapse = " "),
+          call. = FALSE)
+  }
+
+  n <- nrow(seqdata)
+  missing.char <- attr(seqdata,"missing")
+  nr <- attr(seqdata,"nr")
+  void <- attr(seqdata, "void")
+  lgth <- max(seqlength(seqdata))
+  alph <- alphabet(seqdata)
+
+  new.lgth <- ceiling(lgth/tspan)
+  new.lgth.f <- floor(lgth/tspan)
+
+  newseq <- seqdata[,1:new.lgth]
+  cnames <- names(seqdata)
+  newcnames <- cnames[seq(from=1, to=lgth, by=tspan)]
+
+  firstvalid <- function (seq, alphabet, first=TRUE, missing.char=NA) {
+  	pos <- which(seq %in% alphabet)
+  	if (length(pos)==0) res <- missing.char
+  	else {
+      if (first) fpos <- min(pos)
+      else fpos <- max(pos)
+      res <- seq[fpos]
     }
+  	return(res)
+	}
 
-    n <- nrow(seqdata)
-    lgth <- max(seqlength(seqdata))
+  prev <- 0
+  if (method=="first") prev <- tspan - 1
+  for (i in 1:new.lgth.f){
+      newseq[,i] <- seqdata[,tspan*i - prev]
+  }
 
-    new.lgth <- ceiling(lgth/tspan)
-    new.lgth.f <- floor(lgth/tspan)
-
-    newseq <- seqdata[,1:new.lgth]
-    cnames <- names(seqdata)
-    newcnames <- cnames[seq(from=1, to=lgth, by=tspan)]
-
-    prev <- 0
-    if (method=="first") prev <- tspan - 1
+  if (method %in% c("first.valid","last.valid")){
+    first <- method == "first.valid"
     for (i in 1:new.lgth.f){
-        newseq[,i] <- seqdata[,tspan*i - prev]
+      seq <- seqdata[,(tspan*(i-1) + 1):(tspan*i)]
+      newseq[,i] <- apply(seq, 1, firstvalid, alphabet=alph, first=first, missing.char=missing.char)
     }
-    if (method=="mostfreq") {
-        for(i in 1:new.lgth.f) {
-            st.freq <- suppressMessages(seqistatd(seqdata[,(tspan*(i-1) + 1):(tspan*i)]))
-            newseq[,i] <- apply(st.freq,1,function(x){names(which.max(x))})
-        }
-}
-    if (new.lgth > new.lgth.f){
-        if (method=="first"){
-            newseq[,new.lgth] <- seqdata[,tspan*new.lgth.f + 1]
-        } else if (method=="mostfreq") {
-            st.freq <- suppressMessages(seqistatd(seqdata[,(tspan*new.lgth.f+1):lgth]))
-            newseq[,new.lgth] <- apply(st.freq,1,function(x){names(which.max(x))})
-        } else { # method=="last"
-            newseq[,new.lgth] <- seqdata[,lgth]
-        }
-        newcnames[new.lgth] <- cnames[lgth]
-    }
-    colnames(newseq) <- newcnames
-    attr(newseq,"xtstep") <- ceiling(attr(seqdata,"xtstep")/tspan)
-	return(newseq)
+  }
+  if (method=="mostfreq") {
+      for(i in 1:new.lgth.f) {
+          st.freq <- suppressMessages(seqistatd(seqdata[,(tspan*(i-1) + 1):(tspan*i)]))
+          newseq[,i] <- apply(st.freq,1,function(x){ifelse(max(x)==0,missing.char,names(which.max(x)))})
+      }
+  }
+  if (new.lgth > new.lgth.f){
+      if (method=="first"){
+          newseq[,new.lgth] <- seqdata[,tspan*new.lgth.f + 1]
+      } else if (method %in% c("first.valid","last.valid")){
+          first <- method == "first.valid"
+          seq <- seqdata[,(tspan*new.lgth.f+1):lgth]
+          newseq[,new.lgth] <- apply(seq, 1, firstvalid, alphabet=alph, first=first, missing.char=missing.char)
+      } else if (method=="mostfreq") {
+          st.freq <- suppressMessages(seqistatd(seqdata[,(tspan*new.lgth.f+1):lgth]))
+          newseq[,new.lgth] <- apply(st.freq,1,function(x){ifelse(max(x)==0,missing.char,names(which.max(x)))})
+      } else { # method=="last"
+          newseq[,new.lgth] <- seqdata[,lgth]
+      }
+      newcnames[new.lgth] <- cnames[lgth]
+  }
+  if (method %in% c("mostfreq","first.valid","last.valid")) {
+      newseq <- suppressMessages(seqdef(newseq,
+                  alphabet=alph,
+                  weights =attr(seqdata,"weights"),
+                  labels  =attr(seqdata,"labels"),
+                  start   =attr(seqdata,"start"),
+                  missing =attr(seqdata,"missing"),
+                  nr      =attr(seqdata,"nr"),
+                  void    =attr(seqdata,"void"),
+                  #xtstep  =attr(seqdata,"xtstep"),
+                  cpal    =attr(seqdata,"cpal"),
+                  tick.last=attr(seqdata,"tick.last"),
+                  Version =attr(seqdata,"Version")
+                  ))
+  }
+  colnames(newseq) <- newcnames
+  attr(newseq,"xtstep") <- ceiling(attr(seqdata,"xtstep")/tspan)
+	
+  return(newseq)
 }
