3bfc90b6076749314dab05ee991430c716d82a33
gritschard <gritschard@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2021-01-18 15:39:31 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/traminer@1180 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/TraMineRextras/R/seqpolyads.R
+++ pkg/TraMineRextras/R/seqpolyads.R
@@ -4,7 +4,8 @@
 
 
 seqpolyads <- function (seqlist, a=1, method="HAM", ..., w=rep(1,ncol(combn(1:length(seqlist),2))),
-              s=36963, T=1000, core=1, replace=TRUE, with.missing=FALSE, show.time=FALSE) {
+              s=36963, T=1000, core=1, replace=TRUE, weighted=TRUE,
+              with.missing=FALSE, rand.weight.type=1, show.time=FALSE) {
 
   #gc(FALSE)
   if (show.time) ptime.begin <- proc.time()
@@ -44,21 +45,27 @@ seqpolyads <- function (seqlist, a=1, method="HAM", ..., w=rep(1,ncol(combn(1:le
   polyads.dist=polyads.dummy=test.p <- array(0,c(1,n))
 
   seqall <- do.call("rbind", seqlist)
-  alldist <- suppressMessages(seqdist(seqall, method=method, with.missing=with.missing, ...))
+  ## sequence weights
+  ## seqall has non null weights iff each stslist in seqlist has non null weights
+  weights <- if(weighted) attr(seqall,"weights") else NULL
+  if (is.null(weights)) weights <- rep(1,nrow(seqall))
+
+  alldist <- suppressMessages(seqdist(seqall, method=method, with.missing=with.missing, weighted=weighted, ...))
 
   cj=0
   for (p in 2:P) cj[p] <- n * (p-1)
 
   ## Matrix of indexes of sampled sequences per generation
   if (core==1) {
-    l.m <- t(sapply(1:T, function(x) {cj + sample(1:n, size=P, replace=TRUE)}))
-    #l.m <- matrix(NA,T,P)
-    #for (i in 1:T) {
-    #    l.m[i,] <- cj + sample(1:n, P, replace=TRUE)
-    #}
+    #l.m <- t(sapply(1:T, function(x) {cj + sample.int(n, size=P, replace=TRUE,)}))
+    ## sapply is not faster than for
+    l.m <- matrix(NA,T,P)
+    for (i in 1:T) {
+        l.m[i,] <- cj + sample.int(n, size=P, replace=TRUE,  prob=weights[1:n])
+    }
   } else {
     l.m <- foreach (i=1:T, .combine='rbind') %dopar% {
-      t(cj + sample(1:n, P, replace=TRUE))
+      t(cj + sample.int(n, size=P, replace=TRUE, prob=weights[1:n]))
     }
   }
 
@@ -110,16 +117,28 @@ seqpolyads <- function (seqlist, a=1, method="HAM", ..., w=rep(1,ncol(combn(1:le
 
   if (core>1) stopCluster(cl)
 
+  l.weights <- array(1,T)
+  if (weighted){
+    p.weights <- if (rand.weight.type == 2) apply(l.m,2,function(x){sum(weights[x])}) else P
+    for (i in 1:T) {
+      l.weights[i] <- sum(weights[l.m[i,]]/p.weights)
+    }
+  }
+  l.weights <- l.weights/sum(l.weights)
+
   for (j in 1:n) {
     polyads.dist[j] <- sum(alldist[cj+j,cj+j][upper.tri(matrix(NA,P,P))]*w)/d
-    test.p[j] <- sum(polyads.dist[j]<random.dist)/T
+    #test.p[j] <- sum(polyads.dist[j]<random.dist)/T
+    test.p[j] <- sum((polyads.dist[j]<random.dist)*l.weights)
     if (test.p[j]>0.95) polyads.dummy[j] <- 1
     else polyads.dummy[j] <- 0
   }
-  mean.U <- mean(random.dist) - polyads.dist
+  mean.rand.dist <- sum(random.dist*l.weights)
+  mean.U <- mean.rand.dist - polyads.dist
   U.p <- 2*pt(mean.U/{sd(random.dist)/sqrt(T)},T-1,lower.tail=F)
 
-  mean.dist <- c(mean(polyads.dist),mean(random.dist))
+  mean.obs.dist <- sum(polyads.dist*weights[1:n])/sum(weights[1:n])
+  mean.dist <- c(mean.obs.dist,mean.rand.dist)
   names(mean.dist) <- c("Obs","Rand")
 
   if (show.time) print(proc.time()-ptime.begin)
